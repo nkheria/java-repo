0507b38ddf9056756a0c228200a8afe5f9f9445c
nkheria <50733756+nkheria@users.noreply.github.com>
2021-03-25 14:15:23 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -2,7 +2,8 @@ public class Demo {
     public static void main(String[] args){
         int x = 10;
         int y = 20;
+        int z = 30;
 
-        System.out.println(x + y);
+        System.out.println(x + y + z);
     }
 }
